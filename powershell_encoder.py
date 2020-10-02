864533fc0a81ad782d575ba660c80e1bf5f61fa3
Butrint Komoni <butrintkomonih@gmail.com>
2020-10-03 01:08:28 +0200

    Update powershell_encoder.py

--- powershell_encoder.py
+++ powershell_encoder.py
@@ -1,14 +1,14 @@
 import sys, argparse, base64
 
 parser = argparse.ArgumentParser()
-parser.add_argument('command', nargs='?', help='String to encode', type=str)
+parser.add_argument('payload', nargs='?', help='String to encode', type=str)
 parser.add_argument('-s', '--stdin', default=False, help='Reading from stdin', action='store_true')
 args = parser.parse_args()
 
 if args.stdin == True:
 	payload = sys.stdin.read().encode('utf-8')
-elif args.command:
-	payload = args.command.encode('utf-8')
+elif args.payload:
+	payload = args.payload.encode('utf-8')
 else:
 	sys.exit(parser.print_help())
 
